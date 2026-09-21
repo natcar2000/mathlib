301f7a7e406cf41be40aac922c2c31f4dcf5f0c9
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-21 10:13:01 -0300

    Update probability.py

--- mathlib/probability.py
+++ mathlib/probability.py
@@ -13,16 +13,15 @@ def validade_values(favorable_cases, possible_cases):
 
 
 def probability(favorable_cases, possible_cases):
-    if type(favorable_cases) != int or type(possible_cases) != int:
-        raise TypeError("Parameters must have integer values.")
-        
-    if favorable_cases < 0:
-        raise ValueError("Quantity of favorable cases must be at least zero.")
-    
-    if possible_cases <= 0:
-        raise ValueError("Quantity of possible cases must be greater than zero.")
-    
-    if favorable_cases > possible_cases:
-        raise ValueError("Quantity of favorable cases must be smaller of or equal to possible cases.")
-    
+    validate_values(favorable_cases, possible_cases)
     return favorable_cases / possible_cases
+
+
+def complement_probability(probability):
+    if type(probability) not in (int, float):
+        raise TypeError("Probability must have a numeric value.")
+    
+    if not 0 <= probability <= 1:
+        raise ValueError("Probability must be between zero and one.")
+
+    return 1 - probability
