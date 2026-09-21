4738232921386934ba0639d5ff1e4c45e3376e40
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-21 11:37:10 -0300

    Refactor import statements in _Init__.py

--- mathlib/_Init__.py
+++ mathlib/_Init__.py
@@ -1,3 +1,7 @@
-import analytic_geometry, combinatorics, function
-import plan_geometry, probability, spatial geometry
-import statistic
+from . import analytic_geometry
+from . import combinatorics
+from . import function
+from . import plan_geometry
+from . import probability
+from . import spatial_geometry
+from . import statistic
